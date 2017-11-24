befcd9a67122d87ff8e8e84b1d18c5e0c5d9f625
4pydev <4pydev@gmail.com>
2017-11-24 16:18:34 +0300

    Added strip() to load_words()

--- 01/wordvalue.py
+++ 01/wordvalue.py
@@ -3,8 +3,7 @@ from data import DICTIONARY, LETTER_SCORES
 def load_words():
     """Load dictionary into a list and return list"""
     with open(DICTIONARY) as f:
-        words_list = f.read().split()
-    return words_list
+        return [word.strip() for word in f.read().split()]
 
 def calc_word_value(word):
     """Calculate the value of the word entered into function
@@ -19,7 +18,7 @@ def calc_word_value(word):
 def max_word_value(words=load_words()):
     """Calculate the word with the max value, can receive a list
     of words as arg, if none provided uses default DICTIONARY"""
-    return max(words, key=calc_word_value())
+    return max(words, key=lambda word: calc_word_value(word))
 
 if __name__ == "__main__":
     print(max_word_value())
