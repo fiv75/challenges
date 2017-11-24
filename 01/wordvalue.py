87dc50f7551fe8821615a4a54c1f36f33dd4049b
4pydev <4pydev@gmail.com>
2017-11-24 16:57:35 +0300

    Modified funcs in wordvalues.py

--- 01/wordvalue.py
+++ 01/wordvalue.py
@@ -1,26 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 24 15:45:02 2017
+
+@author: fiv75
+"""
 from data import DICTIONARY, LETTER_SCORES
+
 
 def load_words():
     """Load dictionary into a list and return list"""
-    with open(DICTIONARY) as f:
-        return [word.strip() for word in f.read().split()]
+    with open(DICTIONARY) as init_file:
+        return [word.strip() for word in init_file.read().split()]
+
 
 def calc_word_value(word):
     """Calculate the value of the word entered into function
     using imported constant mapping LETTER_SCORES"""
-    word_scores = 0
-    for current_letter in word.upper():
-        for letter, score in LETTER_SCORES.items():
-            if letter == current_letter:
-                word_scores += score
-    return word_scores
+    return sum((LETTER_SCORES.get(letter, 0) for letter in word.upper()))
+
 
 def max_word_value(words=load_words()):
     """Calculate the word with the max value, can receive a list
     of words as arg, if none provided uses default DICTIONARY"""
-    return max(words, key=lambda word: calc_word_value(word))
+    return max(words, key=calc_word_value)
+
 
 if __name__ == "__main__":
     print(max_word_value())
-    
     # run unittests to validate
